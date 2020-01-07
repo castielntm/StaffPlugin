03e7bd51a2fae9eeb20f9c31936e76887111be58
PikachuDev <55398475+OnlyPikachudev@users.noreply.github.com>
2020-01-07 01:25:07 +0100

    Updated version (v 0.11)

--- StaffScript.js
+++ StaffScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nulled Anti-Leech SB
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.11
 // @description  Nulled Anti-Leech SB
 // @author       ChristmasDev
 // @match        https://www.nulled.to/
